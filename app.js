aef65b952b2c80f417da9e001a595efe87e56e92
jsakamoto <junichi.jp@gmail.com>
2016-08-11 21:06:06 +0900

    SE x 3種が鳴らせるところまで実装

--- app.js
+++ app.js
@@ -25,12 +25,23 @@ document.getElementById("BGM1").addEventListener("click", function () {
     bgm.play();
 });
 
-var audio = new Audio();
-audio.src = "assets/SE/SE1.mp3";
+// SE(効果音)について、ボタン要素のIDと、そのボタンをクリックしたときに鳴らすSE音源との対応を定義
+var seDefinitions = [
+    { id: "SE1", src: "assets/SE/SE1.mp3" },
+    { id: "SE2", src: "assets/SE/SE2.mp3" },
+    { id: "SE3", src: "assets/SE/SE3.mp3" },
+];
 
-document.getElementById("SE1").addEventListener("click", function () {
-    if (audio.currentTime > 0) {
-        audio.currentTime = 0;
-    }
-    audio.play();
+// 先の定義に従って、SEボタンクリック時に対応する効果音を鳴らす動作を配線
+seDefinitions.forEach(function (seDefinition) {
+    var audio = new Audio();
+    audio.src = seDefinition.src;
+
+    document.getElementById(seDefinition.id).addEventListener("click", function () {
+        if (audio.currentTime > 0) {
+            audio.currentTime = 0;
+        }
+        audio.play();
+    });
 });
+
